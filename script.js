5c0a2c4d07b1de32203930a4c4c98815efa32818
Viswa Teja <115293958+v-teja@users.noreply.github.com>
2023-04-28 22:14:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species;
+	}
+	get getSpecies{
+	return this.species;
+	}
+	function makeSound{
+		console.log("Animal sound");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(){
+		super(dog);
+	}
+	function bark(){
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(){
+		super(cat);
+	}
+	function purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
